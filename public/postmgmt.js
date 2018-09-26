51207edceed7de6f9ffeaa54847a21eab60b96bf
Vincent Bengtsson <support@mydoomsite.com>
2018-09-26 20:33:49 +0200

    Fixed date display.

--- public/postmgmt.js
+++ public/postmgmt.js
@@ -16,12 +16,13 @@ function documentLoaded() {
             var element = document.getElementById("posts");
             for(var i = 0; i < response.posts.length; i++) {
                 var post = response.posts[i];
+                var timestamp = new Date(post.timestamp);
 
                 var postElement = document.createElement("div");
                 postElement.className = "post";
                 
                 var dateElement = document.createElement("h1");
-                dateElement.innerText = post.timestamp;
+                dateElement.innerText = formatDate(timestamp);
 
                 var textElement = document.createElement("p");
                 textElement.innerText = post.data;
@@ -57,4 +58,15 @@ function post() {
             alert("Invalid response received from server!");
         }
     })
+}
+
+function formatDate(date) {
+    if(typeof date !== "Date") { throw "Input must be a valid Date!"; }
+    return date.getFullYear() + "-" + formatNumber(date.getMonth() + 1) + "-" + formatNumber(date.getDate()) + "  " + 
+            formatNumber(date.getHours()) + ":" + formatNumber(date.getMinutes()) + ":" + formatNumber(date.getSeconds());
+}
+
+function formatNumber(num) {
+    if(typeof num !== "number") { throw "Input must be a valid number!"; }
+    return (num >= 0 && num <= 9 ? "0" + num.toString() : num.toString());
 }
